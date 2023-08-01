5c9151cbc959ba3f4d163a97ec9d93cea8c3007a
vaibdix <dixitvaib@gmail.com>
2023-08-01 16:42:03 +0530

    connection and readme updated

---
+++ admin.js
@@ -0,0 +1,13 @@
+const { kafka } = require("./client");
+
+async function init() {
+  const admin = kafka.admin();
+  console.log("Admin connecting...");
+  admin.connect();
+  console.log("Adming Connection Success...");
+
+  console.log("Disconnecting Admin..");
+  await admin.disconnect();
+}
+
+init();
